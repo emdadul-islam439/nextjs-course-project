5ab8b9e3e7811c1f503e2799bdfd9b6d615fcafd
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-25 12:09:18 +0600

    finished ch-03, vid-08-> extracted dynamic-path's value and path-name

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,9 @@
+import { useRouter } from "next/router";
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+  console.log(router.pathname)
+  console.log(router.query)
   return (
     <div>
       <h1>This is the PortfolioProjectPage</h1>
