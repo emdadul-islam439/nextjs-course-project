192a9f7988db20ad9c585d595f4f55d5f7d372d7
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-25 13:16:00 +0600

    finished ch-03 vid-12-> learned to add dynamic links into Link

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,13 +1,24 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function ClientsPage() {
   const router = useRouter();
   console.log(router.pathname);
   console.log(router.query);
 
+  const clients = [
+    { id: "max", name: "Maximillian" },
+    { id: "manu", name: "Manuel" },
+  ];
+
   return (
     <div>
       <h1>This is the ClientsPage</h1>
+      {clients.map((client) => (
+        <li key={client.id}>
+          <Link href={`/clients/${client.id}`}>{client.name}</Link>
+        </li>
+      ))}
     </div>
   );
 }
