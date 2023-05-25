95134382a3f481b91b19cd19d7cd96d253ec3e0a
Emdadul_Islam <emdadulislam439@gmail.com>
2023-05-25 13:23:13 +0600

    finished ch-03 vid-13-> just refactored the code like the instructor

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -16,7 +16,14 @@ function ClientsPage() {
       <h1>This is the ClientsPage</h1>
       {clients.map((client) => (
         <li key={client.id}>
-          <Link href={{pathname: "/clients/[id]", query: {id: client.id}}}>{client.name}</Link>
+          <Link
+            href={{
+              pathname: "/clients/[id]",
+              query: { id: client.id },
+            }}
+          >
+            {client.name}
+          </Link>
         </li>
       ))}
     </div>
